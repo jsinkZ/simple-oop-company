b1eddf823e5619dba6020d4edddb1998134a7f13
Artemiy <andrikv@gmail.com>
2022-12-09 00:43:51 +0300

    new algorithm for contest in company

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 class Person:
-    def __init__(self, name, age, experience, req_salary):
+    def __init__(self, name, age, experience, req_salary, looking_position):
         self.name = name
         self.age = age
         self.experience = experience  # experience in years
         self.req_salary = req_salary  # requested salary in dollars
+        self.looking_position = (
+            looking_position  # looking for this position in next company
+        )
 
     def say_hi(self):
         print(
@@ -14,15 +17,25 @@ class Person:
             self.experience,
             "years, I request a salary of",
             self.req_salary,
-            "$",
+            "$. Im looking for",
+            self.looking_position,
+            "position in next company",
         )
 
 
 class Worker(Person):
     def __init__(
-        self, name, age, experience, req_salary, working_now, position, current_salary
+        self,
+        name,
+        age,
+        experience,
+        req_salary,
+        looking_position,
+        working_now,
+        position,
+        current_salary,
     ):
-        super().__init__(name, age, experience, req_salary)
+        super().__init__(name, age, experience, req_salary, looking_position)
         self.working_now = working_now.name  # company name
         self.position = position
         self.current_salary = current_salary
@@ -41,21 +54,45 @@ class Worker(Person):
 
 
 class Company:
-    def __init__(self, name, description, max_staff, salary, work_group):
+    def __init__(
+        self, name, description, max_staff, salary, work_group, ratio_over_salary
+    ):
         self.name = name
         self.description = description
         self.max_staff = max_staff  # max count of staff
         self.salary = salary  # staring salary for [junior, middle, senior] in dollars
         self.work_group = work_group  # team workers in this company
+        self.ratio_over_salary = (
+            ratio_over_salary  # the coefficient of overpayment of salary
+        )
 
     # contest among applicants
     def contest(self, applicants, count_places):
+        # if applicants require salary more than min of his position * 0.3 * max coefficient of overpayment of salary he will skipped
+
         people = sorted(
             applicants, key=lambda applicant: applicant.experience, reverse=True
         )
+
         while len(people) and count_places > 0:
-            self.work_group.append(people[0])
-            count_places -= 1
+            applicant_position_index = 0
+
+            # get applicant position index
+            for position_index in range(len(self.salary)):
+                if people[0].looking_position == "junior":
+                    applicant_position_index = position_index
+                if people[0].looking_position == "middle":
+                    applicant_position_index = position_index
+                if people[0].looking_position == "senior":
+                    applicant_position_index = position_index
+
+            if (
+                people[0].req_salary
+                <= self.salary[applicant_position_index] * 0.3 * self.ratio_over_salary
+            ):
+                self.work_group.append(people[0])
+                count_places -= 1
+
             del people[0]
 
     # print current work group
@@ -79,7 +116,6 @@ class Company:
     # employ without contest in company
     def employ(self, person):
         self.work_group.append(person)
-        self.current_group()
 
     # fire person
     def fire(self, person):
@@ -89,7 +125,6 @@ class Company:
                 del group[i]
                 break
         self.work_group = group
-        self.current_group()
 
 
 """
@@ -100,33 +135,40 @@ class Company - company -> (name, description, max_staff, salary, work_group)
 
 # * companies
 VVS_company = Company(
-    "VVS company", "Best solve for your business", 30, [1000, 3000, 4500], []
+    "VVS company", "Best solve for your business", 30, [1000, 3000, 4500], [], 1.45
 )
 Diamonds_crime_company = Company(
-    "Diamonds crime", "Design UI", 12, [800, 2200, 3700], []
+    "Diamonds crime", "Design UI", 12, [800, 2200, 3700], [], 1.25
 )
 
 # * people
-dan = Person("Dan", 25, 0, 1200)
-jacob = Person("Jacob", 20, 0, 700)
-ben = Person("Ben", 24, 1, 950)
-john = Person("John", 27, 3, 1200)
-wilson = Person("Wilson", 39, 17, 6600)
+dan = Person("Dan", 25, 0, 1200, "junior")
+jacob = Person("Jacob", 20, 0, 700, "junior")
+ben = Person("Ben", 24, 3, 950, "middle")
+john = Person("John", 27, 5, 1200, "senior")
+wilson = Person("Wilson", 39, 17, 6600, "senior")
 
 # * workers
-bruce = Worker("Bruce", 29, 7, 1800, Diamonds_crime_company, "junior", 1200)
-jack = Worker("Jack", 29, 7, 3900, Diamonds_crime_company, "middle", 3700)
-alex = Worker("Alex", 32, 10, 6400, Diamonds_crime_company, "senior", 6000)
+bruce = Worker("Bruce", 23, 3, 1800, "middle", Diamonds_crime_company, "junior", 1200)
+jack = Worker("Jack", 29, 7, 3900, "senior", Diamonds_crime_company, "middle", 3700)
+alex = Worker("Alex", 32, 10, 6400, "senior", Diamonds_crime_company, "senior", 6000)
 
 # * testing
 
 # john.say_hi()
 # jack.say_hi()
-wilson.say_hi()
-alex.say_hi()
+# wilson.say_hi()
+# alex.say_hi()
 
-VVS_company.about_company()
-Diamonds_crime_company.about_company()
+# VVS_company.about_company()
+# Diamonds_crime_company.about_company()
 
-# some_ceo.contest([jack, john, ben, wilson, jacob], 3)
-# some_ceo.current_group()
+Diamonds_crime_company.employ(bruce)
+Diamonds_crime_company.employ(jack)
+Diamonds_crime_company.employ(alex)
+
+Diamonds_crime_company.current_group()
+
+VVS_company.current_group()
+VVS_company.contest([dan, jacob, ben, john, wilson, bruce, jack, alex], 5)
+VVS_company.current_group()
